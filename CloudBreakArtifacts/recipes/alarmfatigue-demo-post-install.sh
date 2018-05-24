2ea9c3f65536847a379959a6668197874012dc95
Taylor Herson <therson@hortonworks.com>
2018-05-24 14:07:24 -0700

    sh

--- CloudBreakArtifacts/recipes/alarmfatigue-demo-post-install.sh
+++ CloudBreakArtifacts/recipes/alarmfatigue-demo-post-install.sh
@@ -92,7 +92,7 @@ getServiceStatus () {
        	SERVICE=$1
        	SERVICE_STATUS=$(curl -u admin:admin -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/$SERVICE | grep '"state" :' | grep -Po '([A-Z]+)')
 
-       	#echo "*********************************$SERVICE STATUS IS $SERVICE_STATUS"
+       	echo $SERVICE_STATUS
 }
 
 waitForService () {
@@ -137,7 +137,7 @@ waitForServiceToStart () {
 
 stopService () {
        	SERVICE=$1
-       	getServiceStatus $SERVICE
+       	SERVICE_STATUS=$(getServiceStatus $SERVICE)
        	echo "*********************************Stopping Service $SERVICE ..."
        	if [ "$SERVICE_STATUS" == STARTED ]; then
         TASKID=$(curl -u admin:admin -H "X-Requested-By:ambari" -i -X PUT -d "{\"RequestInfo\": {\"context\": \"Stop $SERVICE\"}, \"ServiceInfo\": {\"maintenance_state\" : \"OFF\", \"state\": \"INSTALLED\"}}" http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/$SERVICE | grep "id" | grep -Po '([0-9]+)')
@@ -161,7 +161,7 @@ stopService () {
 
 startService (){
        	SERVICE=$1
-       	getServiceStatus $SERVICE
+       	SERVICE_STATUS=$(getServiceStatus $SERVICE)
        	echo "*********************************Starting Service $SERVICE ..."
        	if [ "$SERVICE_STATUS" == INSTALLED ]; then
         TASKID=$(curl -u admin:admin -H "X-Requested-By:ambari" -i -X PUT -d "{\"RequestInfo\": {\"context\": \"Start $SERVICE\"}, \"ServiceInfo\": {\"maintenance_state\" : \"OFF\", \"state\": \"STARTED\"}}" http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/$SERVICE | grep "id" | grep -Po '([0-9]+)')
@@ -184,7 +184,7 @@ startService (){
 
 startServiceAndComplete (){
        	SERVICE=$1
-       	getServiceStatus $SERVICE
+       	SERVICE_STATUS=$(getServiceStatus $SERVICE)
        	echo "*********************************Starting Service $SERVICE ..."
        	if [ "$SERVICE_STATUS" == INSTALLED ]; then
         TASKID=$(curl -u admin:admin -H "X-Requested-By:ambari" -i -X PUT -d "{\"RequestInfo\": {\"context\": \"INSTALL COMPLETE\"}, \"ServiceInfo\": {\"maintenance_state\" : \"OFF\", \"state\": \"STARTED\"}}" http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/$SERVICE | grep "id" | grep -Po '([0-9]+)')
